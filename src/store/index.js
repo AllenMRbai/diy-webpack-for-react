187aaeaf1733d1407c1efa6900c3fb79f7a7652f
Allen Bai <957820957@qq.com>
2019-03-16 20:20:00 +0800

    add redux devtool extension support

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { counter } from "./counter.redux";
 import { user } from "./user.redux";
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 export default createStore(
   combineReducers({ counter, user }),
-  applyMiddleware(thunk)
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
